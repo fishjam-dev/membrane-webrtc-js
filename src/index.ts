e88e525fd1c96c9c0246e6889aff350e8fc7fdc2
Michał Śledź <sledzson08@gmail.com>
2022-04-28 12:53:52 +0200

    Copy files from `membrane_rtc_engine`

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * some test docs
+ * @module
+ */
+
+export {
+  BandwidthLimit,
+  Callbacks,
+  MembraneWebRTCConfig,
+  MembraneWebRTC,
+  Peer,
+  SimulcastConfig,
+  TrackContext,
+  TrackEncoding,
+} from "./membraneWebRTC";
+
+export { SerializedMediaEvent } from "./mediaEvent";

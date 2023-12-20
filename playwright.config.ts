6b73efca4e52ccfa4f42507bdacd495ffd232cfb
Kamil Stasiak <kamilstasiaknew@gmail.com>
2023-12-20 19:17:20 +0100

    Move e2e test to e2e catalog

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./e2e",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? undefined : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -84,9 +84,9 @@ export default defineConfig({
     command: "npm run dev",
     url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
-    cwd: "tests/app",
+    cwd: "e2e/app",
   },
 
-  globalSetup: "./tests/setup/setupJellyfish",
-  globalTeardown: "./tests/setup/teardownJellyfish",
+  globalSetup: "./e2e/setup/setupJellyfish",
+  globalTeardown: "./e2e/setup/teardownJellyfish",
 });
